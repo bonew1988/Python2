db70f87c8acee035c2a9659910cf39de938daf6e
bonew1988 <bonew@bonew.ru>
2023-08-21 22:22:33 +0300

    add seminar 8

---
+++ newPy/seminars/task8/task1.py
@@ -0,0 +1,21 @@
+# Задание №1
+# Вспоминаем задачу 3 из прошлого семинара. Мы сформировали
+# текстовый файл с псевдо именами и произведением чисел.
+# Напишите функцию, которая создаёт из созданного ранее
+# файла новый с данными в формате JSON.
+# Имена пишите с большой буквы.
+# Каждую пару сохраняйте с новой строки.
+import json
+
+
+def txt_to_json(input_file: str, output_file: str):
+    with open(input_file, 'r') as f:
+        data = f.read(). split('\n')[:-1]
+    data = [{i.split()[0].capitalize(): i.split()[1]} for i in data]
+    print(data)
+    with open(output_file, 'w') as res:
+        json.dump(data, res, indent=4)
+
+
+if __name__ == '__main__':
+    txt_to_json('file_output.txt', 'outputfile.json')
---
+++ newPy/seminars/task8/task3.py
@@ -0,0 +1,45 @@
+# Задание №3
+# Напишите функцию, которая сохраняет созданный в
+# прошлом задании файл в формате CSV
+
+import json
+import csv
+
+
+def json_to_csv(filename: str):
+    with open(f'{filename}.json', 'r') as f_inp:
+        data = json.load(f_inp)
+    rows = []
+    for level, users in data.items():
+        for id, name in users.items():
+            rows.append({'level': level,
+                         'name': name,
+                         'id': id})
+    with open(f'{filename}.csv', 'w', newline='') as res:
+        csv_write = csv.DictWriter(res, fieldnames=['level',
+                                                    'name',
+                                                    'id'])
+        csv_write.writeheader()
+        csv_write.writerows(rows)
+
+
+def csv_to_json(filename: str):
+    with open(f'{filename}.csv', 'r', newline='') as f_csv:
+        data = f_csv.read().split('\n')
+        print(data)
+
+    res: list = []
+    data.pop()
+    for value in data[1:]:
+        print(value)
+        level, name, id = value[:-1].split(',')
+        res.append({"id": f"{int(id):06}", "level": level,
+                   "name": name, "hash": hash(id+name)})
+
+    with open(f'task5_{filename}.json', 'w', newline='') as f_json:
+        json.dump(res, f_json, indent=4)
+
+
+if __name__ == '__main__':
+    json_to_csv('users')
+    csv_to_json('users')
